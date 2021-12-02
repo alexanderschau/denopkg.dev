1920d7afd5e82c68b16ce445b88ffbfcb8223931
Alexander Schau <alexander@schau.email>
2021-12-02 16:48:36 +0000

    add cloudflare pages adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import cloudflare from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,7 +13,7 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter(),
+		adapter: cloudflare(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
