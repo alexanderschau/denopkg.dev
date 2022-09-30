dac55177de800925d494bab20ff625c1b5bf104c
Alexander Schau <alexander@schau.email>
2022-09-30 07:43:02 +0000

    add custom gitlab support to ui

--- src/lib/tool.svelte
+++ src/lib/tool.svelte
@@ -1,8 +1,9 @@
-<script>
+<script lang="ts">
 	import { copyToClipboard } from './fn';
 
 	let url = '';
 	let type = '';
+	let server: string | null = null;
 	let owner = '';
 	let repo = '';
 	let tag = '';
@@ -13,10 +14,13 @@
 			/github\.com\/(.*?)\/([^?# \/]+?)(?:\/[^\/]*\/([^\/]*)(?:\/([^?#]+))?)?(?:\.git.*|[?# \/].*|)$/;
 		const checkGitLab =
 			/gitlab\.com\/(.*?)\/([^?# \/]+?)(?:\/-\/[^\/]*\/([^\/]*)(?:\/([^?#]+))?)?(?:\.git.*|[?# \/].*|)$/;
+		const checkCustomGitLab =
+			/(?:https:\/\/)?([^/]+)\/(.*?)\/([^?# \/]+?)(?:\/-\/[^\/]*\/([^\/]*)(?:\/([^?#]+))?)?(?:\.git.*|[?# \/].*|)$/;
 		console.log(url);
 		const checkGh = url.match(checkGitHub);
 		if (checkGh) {
 			type = 'gh';
+			server = null;
 			owner = checkGh[1] || '';
 			repo = checkGh[2] || '';
 			tag = checkGh[3] || '';
@@ -25,10 +29,20 @@
 		const checkGl = url.match(checkGitLab);
 		if (checkGl) {
 			type = 'gl';
+			server = null;
 			owner = checkGl[1] || '';
 			repo = checkGl[2] || '';
 			tag = checkGl[3] || '';
 			path = checkGl[4] || '';
+		}
+		const checkCustomGl = url.match(checkCustomGitLab);
+		if (checkCustomGl) {
+			type = 'gl';
+			server = checkCustomGl[1] || '';
+			owner = checkCustomGl[2] || '';
+			repo = checkCustomGl[3] || '';
+			tag = checkCustomGl[4] || '';
+			path = checkCustomGl[5] || '';
 		}
 	};
 	$: url, updateData();
@@ -55,19 +69,22 @@
 />
 <p class="my-5">Import the following URL in Deno:</p>
 <div class="text-left whitespace-normal">
-	<span>https://denopkg.dev/</span><span class="microbox">{type || 'type'}</span>/<span
-		class="microbox">{owner || 'user'}</span
-	>/<span class="microbox">{repo || 'repo'}</span>@<span class="microbox"
-		>{tag || 'tag or branch'}</span
-	>/<span class="microbox">{path || 'path/to/file'}</span>
+	<span>https://denopkg.dev/</span><span class="microbox">{type || 'type'}</span>
+	{#if server !== null}
+		/ <span class="microbox">@{server || 'example.com'}</span>
+	{/if}/<span class="microbox">{owner || 'user'}</span>/<span class="microbox"
+		>{repo || 'repo'}</span
+	>@<span class="microbox">{tag || 'tag or branch'}</span>/<span class="microbox"
+		>{path || 'path/to/file'}</span
+	>
 </div>
 <a
 	on:click={(e) => {
 		e.preventDefault();
 		copyToClipboard(
-			`https://denopkg.dev/${type || 'type'}/${owner || 'user'}/${repo || 'repo'}@${
-				tag || 'tag or branch'
-			}/${path || 'path/to/file'}`
+			`https://denopkg.dev/${type || 'type'}${server !== null ? '/@' + server : ''}/${
+				owner || 'user'
+			}/${repo || 'repo'}@${tag || 'tag or branch'}/${path || 'path/to/file'}`
 		);
 	}}
 	href="/"
